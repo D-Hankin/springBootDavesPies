88e547a1417362f5adb5bc31b62fb23e222412ea
David Hankin <d_hankin@outlook.com>
2023-10-25 10:03:44 +0200

    links between pages

--- davesPies/src/main/java/davesPies/davesPies/DavesPiesController.java
+++ davesPies/src/main/java/davesPies/davesPies/DavesPiesController.java
@@ -1,9 +1,36 @@
 package davesPies.davesPies;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
 
 @Controller 
 
 public class DavesPiesController {
+
+    private static final List<String> pies = new ArrayList<>();
+    private static final List<String> employees = new ArrayList<>();
+
+    @GetMapping("/index")
+    String getIndex(Model model) {
+        System.out.println("Home");
+        return "/";
+    }
+
+    @GetMapping("/products")
+    String getProducts(Model model) {
+        System.out.println("products");
+        return "/products";
+    }
+
+    @GetMapping("/employees")
+    String getEmployees(Model model) {
+        System.out.println("employees");
+        return "/employees";
+    }
     
 }
